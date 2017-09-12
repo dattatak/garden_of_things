f140d814d1049a05b89c705ca9c25b2ea5c64088
jefferydotdan <jefferydotdan@gmail.com>
2017-09-12 23:56:39 +0100

    php fixes

--- raspberrypi/web/latestValues.php
+++ raspberrypi/web/latestValues.php
@@ -11,11 +11,8 @@ function values($pin) {
     $nameStmt = $db->prepare("SELECT sample FROM moistureValues WHERE pinNumber =:id ORDER BY rowid DESC LIMIT 30");
     $nameStmt->bindValue(':id', $pin);
     $result = $nameStmt->execute();
-
-    for ($x = 0; $x <= $result->numColumns(); $x++){
-        $res = $result->fetchArray();
-        array_push($resultarr, $res[0]);
-    }
-    echo json_encode($resultarr, JSON_FORCE_OBJECT);
+    
+    
+    echo json_encode($result->fetchAll(), JSON_FORCE_OBJECT);
 }
 ?>
