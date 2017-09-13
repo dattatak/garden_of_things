4cd78c527e2d4d053566085b36b1b783a8fc6047
jefferydotdan <jefferydotdan@gmail.com>
2017-09-13 17:44:24 +0100

    change method

--- raspberrypi/web/latestValues.php
+++ raspberrypi/web/latestValues.php
@@ -3,13 +3,14 @@
 if (isset($_GET['pin'])) {
 	echo values($_GET['pin']);
 }
-function values($pin) {
+function values($pin, $lim) {
 
 	$db = new SQLite3('/var/www/sensors.db');
 	
 	$resultarr = array();
-    $nameStmt = $db->prepare("SELECT sample FROM moistureValues WHERE pinNumber =:id ORDER BY rowid DESC LIMIT 1");
+    $nameStmt = $db->prepare("SELECT sample FROM moistureValues WHERE pinNumber =:id ORDER BY rowid DESC LIMIT :limit");
     $nameStmt->bindValue(':id', $pin);
+    $nameStmt->bindValue(':limit', $lim);
     $result = $nameStmt->execute();
     
     $nrows = 0;
